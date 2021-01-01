4df806414a8acf39ee7fb40b17267bd50cfb5189
Cioscos <claudiocicca1998@gmail.com>
2021-01-01 12:54:19 +0100

    Code refactoring and improving performance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Grid import *
 
 
+# Draw all the pixels gave in grid
 def draw_points(win, grid):
     for pixel in grid:
         pixel.point.draw(win)
@@ -15,12 +16,11 @@ def bubble_sort(win, grid, txt):
         for j in range(n - i - 1):
             no_swapped = True
             txt.setText("swap=%d" % swap)
-            # time.sleep(0.01)
             if grid.pixel_grid[j].value > grid.pixel_grid[j + 1].value:
                 grid.pixel_grid[j], grid.pixel_grid[j + 1] = grid.pixel_grid[j + 1], grid.pixel_grid[j]
                 swap += 1
-                grid.pixel_grid[j].point.x, grid.pixel_grid[j].point.y = j, j
-                grid.pixel_grid[j + 1].point.x, grid.pixel_grid[j + 1].point.y = j + 1, j + 1
+                grid.pixel_grid[j].point.x = grid.pixel_grid[j].point.y = j
+                grid.pixel_grid[j + 1].point.x = grid.pixel_grid[j + 1].point.y = j + 1
 
                 already_sorted = False
                 no_swapped = False
@@ -28,12 +28,12 @@ def bubble_sort(win, grid, txt):
             if i == 0:
                 grid.pixel_grid[j].point.undraw()
                 if no_swapped:
-                    grid.pixel_grid[j].point.x, grid.pixel_grid[j].point.y = j, j
+                    grid.pixel_grid[j].point.x = grid.pixel_grid[j].point.y = j
                 grid.pixel_grid[j].point.draw(win)
 
                 if j == n - i - 2:
                     if no_swapped:
-                        grid.pixel_grid[j + 1].point.x, grid.pixel_grid[j + 1].point.y = j + 1, j + 1
+                        grid.pixel_grid[j + 1].point.x = grid.pixel_grid[j + 1].point.y = j + 1
                     grid.pixel_grid[j + 1].point.undraw()
                     grid.pixel_grid[j + 1].point.draw(win)
             else:
@@ -46,23 +46,36 @@ def bubble_sort(win, grid, txt):
 
 
 def main():
+    # One of the 2 dimension of the window. (Window must be a square)
+    square_size = int(input('Insert one dimension of the square: '))
+
     # Window settings
-    win = GraphWin('Sorter', 500, 500)
+    win = GraphWin('Sorter', square_size, square_size)
     win.setBackground('black')
 
     # Text settings
-    txt = Text(Point((500 / 2) + (500 / 4), 20), "swap=0")
+    txt = Text(Point((square_size / 2) + (square_size / 4), 20), "swap=0")
     txt.setTextColor('white')
     txt.draw(win)
+    txt_for_pause = Text(Point(square_size/2, square_size/2), 'Click on the screen to start')
+    txt_for_pause.setTextColor('white')
 
-    grid = Grid(500)
+    # Grid settings
+    grid = Grid(square_size)
     grid.shuffle()
 
     # Draws all the point on the canvas
     draw_points(win, grid.pixel_grid)
 
+    # Draws a text befor starting the sort
+    txt_for_pause.draw(win)
+    win.getMouse()
+    txt_for_pause.undraw()
+
     # Starts the bubble sorting
     bubble_sort(win, grid, txt)
+
+    # Wait for user input
     win.getMouse()
     win.close()
 
